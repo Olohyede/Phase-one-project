e11b3984a37b5ff5639ad02f580df382db3a0a2a
Olohyede <oloyedet70@gmail.com>
2024-01-24 00:59:41 +0100

    fixed the sum total for the cart

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -33,11 +33,24 @@ const ShopContextProvider = (props) => {
         let itemInfo = all_product.find((product)=>product.id===Number(item))
         totalAmount += itemInfo.new_price * cartItems[item];
       }
-      return totalAmount;
+     
     }
+    return totalAmount; 
   }
 
-  const contextValue = { getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
+  const getTotalCartItems = () =>{
+    let totaltem = 0;
+    for(const item in cartItems)
+    {
+      if(cartItems[item]>0)
+      {
+        totaltem+= cartItems[item];
+      }
+    }
+    return totaltem;
+  }
+
+  const contextValue = { getTotalCartItems,getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
